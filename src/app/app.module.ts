112d17c21c795869d9bbd9ac305c72a49a4533be
esyugcha <esyugcha@espol.edu.ec>
2020-12-09 14:14:45 -0500

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChatComponent } from './chat/chat.component';
 import { ContainerComponent } from './container/container.component';
+import { DatosComponent } from './datos/datos.component';
+import {​​​ Routes, RouterModule }​​​​​​​​​​ from '@angular/router';
+
+const rutas: Routes = [
+  {path: "", component: ContainerComponent}​​​​​​​​​​​​​​​​​​​​​,{path: "datos",component: DatosComponent}​​​​​​​
+];
+
+RouterModule.forRoot(rutas)
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChatComponent, ContainerComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, ChatComponent, ContainerComponent, DatosComponent ],
+  bootstrap:    [ AppComponent ],  
 })
-export class AppModule { }
+export class AppModule {}
